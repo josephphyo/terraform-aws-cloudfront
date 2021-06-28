0becdf6f2df719cc75346e813eb6735e89734294
josephphyo <phyominhtun1990@gmail.com>
2021-06-28 15:25:04 +0630

    first commit

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "cloudfont-domain-name" {
-  value = aws_cloudfront_distribution.s3_distribution.domain_name
+  value       = aws_cloudfront_distribution.s3_distribution.domain_name
   description = "CloudFront Domain Name"
 }
