412562a71fde31024e58d2a9cad2b2d5a328493e
josephphyo <phyominhtun1990@gmail.com>
2021-06-28 20:08:29 +0630

    feat: fixed output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "cloudfont-domain-name" {
+output "cloudfront-domain-name" {
   value       = aws_cloudfront_distribution.s3_distribution.domain_name
   description = "CloudFront Domain Name"
 }
